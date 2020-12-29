cd76429b420efbc960fb610026741df56cebc480
axelcinco <73139260+axelcinco@users.noreply.github.com>
2020-12-29 14:32:33 -0700

    segundo avance del proyecto
    segundo avance del proyecto

--- finanzas.cpp
+++ finanzas.cpp
@@ -4,17 +4,18 @@
  
   using namespace std;
   int opc,opc2;
-  double monto=0,saldo_efe=0,saldo_tarj=0,saldo_tot=0;
+  double monto=0,saldo_efe=0,saldo_tarj=0,saldo_tot=0,saldo_aho=0;
   
   
   double ingresos()
   {
   	
   	cout<<"ingrese el monto del ingreso: ";cin>>monto;
-  	cout<<" 1.- pagos en efectivo: "<<endl;
-  	cout<<"2.- pagos con tarjeta: "<<endl;
-  	cout<<" 3.- salir: "<<endl;
-  	cin>>opc2;
+  	cout<<"1.-pagos en efectivo: "<<endl;
+  	cout<<"2.-pagos con tarjeta: "<<endl;
+  	cout<<"3.-ahorros "<<endl;
+  	cout<<"4.-salir: "<<endl;
+  	cout<<"digite la opcion que desea: ";cin>>opc2;
   	switch(opc2)
   	{
   	   case 1:
@@ -29,21 +30,109 @@
 	  	   cout<<endl;
 	       break;	
 	    case 3:
-	    	break;
+	    	saldo_aho=saldo_aho+monto;
+	    	cout<<" saldo en ahorro es :"<<saldo_aho;
+	  	   cout<<endl;
 	    	break;
 	    	case 4:
-	    		cout<<"opcion invalida "<<endl;
+	    	     break;
 	    		break;
+	     case 5:
+	     	cout<<"opcion invalida "<<endl;
+	     	break;
+	    		
   		
     }
   	
-  	saldo_tot=saldo_tarj+saldo_efe;
+  	saldo_tot=saldo_tarj+saldo_efe+saldo_aho;
   	
   	
   	
   };//aqui termina la funcion ingresos 
   
-  
+  double reti()
+  {
+  	
+  	cout<<"ingrese el monto que desee retirar ";cin>>monto;
+    
+    if(saldo_tot<monto)
+    {
+    	cout<<"no tiene suficiente saldo para retirar esta en numeros rojos "<<endl;
+    	
+	}
+  	else
+  	{
+  	cout<<" 1.- pagos en efectivo: "<<endl;
+  	cout<<"2.- pagos con tarjeta: "<<endl;
+  	cout<<" 3.- ahorros "<<endl;
+  	cout<<" 4.- salir: "<<endl;
+  	cout<<"digite la opcion que desea: ";cin>>opc2;
+  	 switch(opc2)
+  	 {
+  	 	case 1:
+  	 		if(saldo_efe<monto)
+  	 		{
+  	 			cout<<"no tienes efectivo suficiente estas en numeros rojos "<<endl;
+  	 			
+		    }
+		    else
+		    {
+		    saldo_efe=saldo_efe-monto; 
+  	       	cout<<" saldo en efectivo es: "<< saldo_efe;
+  	   	    cout<<endl;
+  	   	    
+  	   	    
+			}
+  	 
+  	   	break;
+  	   	      
+  	   	case 2:
+  	   	if(saldo_tarj<monto)
+  	   	{
+  	   		cout<<"no tiene suficiente saldo en la tarjeta esta en numeros rojos "<<endl;
+	    }
+	    else
+	    {
+	   	saldo_tarj=saldo_tarj-monto;
+	  	cout<<" saldo en trajeta es :"<<saldo_tarj;
+	  	cout<<endl;
+	  	
+		}
+	     
+  	   	break;
+  	   	
+  	   	case 3:
+  	   	   if(saldo_aho<monto)
+  	   	   {
+  	   	   	 cout<<" no tienes suficiente ahorro en  tu cuenta "<<endl;
+		   }
+		   else
+		   {
+		   	saldo_aho=saldo_aho-monto;
+	  	    cout<<" saldo en ahorro es :"<<saldo_aho;
+	  	    cout<<endl;
+	  	    
+		   }
+		  	break;
+  	  case 4 :
+  	  	break;
+  	  	break;
+  	  	
+  	  	default:
+  	  		cout<<"opcion invalida digite otra "<<endl;
+  	  		break;
+  	   	      
+  	 	
+	}//aqui termina el switch
+	
+	 
+	 
+  		
+	}//aqui termina la comparacion else 
+  	
+  	
+  	
+  };//aqui termina la funcion retiro 
   
    
    
@@ -53,17 +142,27 @@ int main()
 {
 	system("color 0b");
   cout<<" bienvenido usuario : axel garcia"<<endl;
+  cout<<"_______________________________________"<<endl;
+  
   
   
   do
   {
-  	cout<<"su saldo total es: "<<saldo_tot;
-    cout<<endl;
-  	cout<<" digite 1: ingresos:"<<endl;
-  	cout<<" digite 2: retiro: "<<endl;
-  	cout<<" digite 3: transferencia: "<<endl;
-  	cout<<" digite 4: salir: "<<endl;
-  	cin>>opc;
+  	
+    cout<<"total en efectivo que tiene: "<<saldo_efe<<endl;
+    cout<<"total de tarjeta que tiene es: "<<saldo_tarj<<endl;
+    cout<<"total en ahorros que tiene es: "<<saldo_aho<<endl;
+    saldo_tot=saldo_efe+saldo_tarj+saldo_aho;
+    cout<<"el saldo total que tiene en su cuenta es de : "<<saldo_tot<<endl;
+    
+    cout<<"______________________________"<<endl;
+  	cout<<"digite 1: ingresos:"<<endl;
+  	cout<<"digite 2: retiro: "<<endl;
+  	cout<<"digite 3: transferencia: "<<endl;
+  	cout<<"digite 4: salir: "<<endl;
+  	cout<<"digite su opcion: ";cin>>opc;
+
+  	
   	system("cls");
   	switch(opc)
   	{
@@ -74,7 +173,7 @@ int main()
   		     
   		case 2 :
   			 cout<<"retiros "<<endl;
-  			 
+  			 reti();
   			 break;//aqui termina el caso 2
   		      
   		    
