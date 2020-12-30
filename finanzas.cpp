179df9551fd50f3eb1815783a771d63a982ab169
axelcinco <73139260+axelcinco@users.noreply.github.com>
2020-12-30 12:11:13 -0700

    tercer avance del proyecto de finanzas
    tercer avance del proyecto

--- finanzas.cpp
+++ finanzas.cpp
@@ -3,9 +3,9 @@
 #include<stdlib.h>
  
   using namespace std;
-  int opc,opc2;
-  double monto=0,saldo_efe=0,saldo_tarj=0,saldo_tot=0,saldo_aho=0;
-  
+  int opc,opc2,opc3;
+  double monto=0,saldo_efe=0,saldo_tarj=0,saldo_tot=0,saldo_aho=0,tot_comida=0,tot_agulu=0,tot_otros=0;
+  int contador=0;
   
   double ingresos()
   {
@@ -77,14 +77,48 @@
 		    }
 		    else
 		    {
-		    saldo_efe=saldo_efe-monto; 
-  	       	cout<<" saldo en efectivo es: "<< saldo_efe;
-  	   	    cout<<endl;
+		   
+		cout<<" digite 1.- comida "<<endl;
+		cout<<"digite 2.- agua o luz "<<endl;
+		cout<<" digite 3.- otros recursos "<<endl;
+		cout<<" en que se efectuo su pago???: ";cin>>opc3;
+		
+		switch(opc3)
+		{
+			 
+			case 1:
+				     tot_comida=tot_comida+monto;
+				      saldo_efe=saldo_efe-monto; 
+				     cout<< "total gastado en comida es de: "<<tot_comida<<endl;
+				     break;
+			 case 2: 
+			       tot_agulu=tot_agulu+monto;
+			        saldo_efe=saldo_efe-monto; 
+			       cout<< "total gastado en agua o luz es de: "<<tot_agulu<<endl;
+			 break;
+			 
+			 case 3: 
+			         tot_otros=tot_otros+monto;
+			         saldo_efe=saldo_efe-monto; 
+			        cout<<" el total gastado en otros recuros es de: "<<tot_otros<<endl;
+			         break;
+			
+			default:
+				     cout<<" opcion invalida digite otra vez "<< endl;
+				     break;
+			           
+		
+			
+		}//aqui termina el switch
+		  
+		    
+  	       	cout<<" saldo en efectivo es: "<< saldo_efe<<endl;
   	   	    
   	   	    
-			}
+  	   	    
+			}//cierre del else caso de la comparacion del monto
   	 
-  	   	break;
+  	   	break;//aqui termina el caso 1
   	   	      
   	   	case 2:
   	   	if(saldo_tarj<monto)
@@ -93,13 +127,43 @@
 	    }
 	    else
 	    {
-	   	saldo_tarj=saldo_tarj-monto;
+	   
+	   	cout<<" digite 1.- comida "<<endl;
+		cout<<"digite 2.- agua o luz "<<endl;
+		cout<<" digite 3.- otros recursos "<<endl;
+		cout<<" en que se efectuo su pago???: ";cin>>opc3;
+		switch(opc3)
+		{
+			case 1: 
+		   saldo_tarj=saldo_tarj-monto;
+		   tot_comida=tot_comida+monto;
+		   cout<<"el total gastado en comida  es de:"<<tot_comida<<endl;
+		   break;
+		   case 2:
+		   	   tot_agulu=tot_agulu+monto;
+		   	   saldo_tarj-monto;
+		   	cout<< "total gastado en agua o luz es de: "<<tot_agulu<<endl; 
+		   	break;
+		   	
+		   	case 3: 
+		   	  tot_otros=tot_otros+monto;
+			   saldo_tarj=saldo_tarj-monto; 
+			   cout<<" el total gastado en otros recuros es de: "<<tot_otros<<endl;
+			    break;
+		   	default:
+		   		  cout<<"opcion invalida "<<endl;
+		   		  
+		   		break;
+			
+		}//aqui termina el switch
+	   	  
+	   	
 	  	cout<<" saldo en trajeta es :"<<saldo_tarj;
 	  	cout<<endl;
 	  	
 		}
 	     
-  	   	break;
+  	   	break;//aqui termina el caso 2
   	   	
   	   	case 3:
   	   	   if(saldo_aho<monto)
@@ -108,12 +172,39 @@
 		   }
 		   else
 		   {
-		   	saldo_aho=saldo_aho-monto;
+		   	cout<<" digite 1.- comida "<<endl;
+		    cout<<"digite 2.- agua o luz "<<endl;
+		    cout<<" digite 3.- otros recursos "<<endl;
+		    cout<<" en que se efectuo su pago???: ";cin>>opc3;
+		    switch(opc3)
+		    
+		    {
+		    	case 1: 
+		    	     tot_comida=tot_comida+monto;
+		    	     saldo_aho=saldo_aho-monto;
+		    	    cout<< "total gastado en comida es de: "<<tot_comida<<endl;
+		    	     break;
+		    	case 2:
+		    		    saldo_aho=saldo_aho-monto;
+		    		    tot_agulu=tot_agulu+monto;
+		    		    cout<< "total gastado en agua o luz es de: "<<tot_agulu<<endl; 
+		    		     break;
+		    	case 3: 
+		    	         saldo_aho=saldo_aho-monto;
+		    		    tot_otros=tot_otros+monto;
+		    		    cout<< "total gastado en otros recursos: "<<tot_otros<<endl; 
+		    	        break;
+		    	
+			}//aqui termina el switch de hacia donde va el dinero 
+		   	
+		   	
+		   	
+		   	
 	  	    cout<<" saldo en ahorro es :"<<saldo_aho;
 	  	    cout<<endl;
 	  	    
 		   }
-		  	break;
+		  	break; //aqui termina el caso 3
   	  case 4 :
   	  	break;
   	  	break;
@@ -148,16 +239,25 @@ int main()
   
   do
   {
-  	
+  	contador=contador+1;
+  	cout<<"numeros de transferencias realizadas en el dia: "<<contador<<endl;
     cout<<"total en efectivo que tiene: "<<saldo_efe<<endl;
     cout<<"total de tarjeta que tiene es: "<<saldo_tarj<<endl;
     cout<<"total en ahorros que tiene es: "<<saldo_aho<<endl;
     saldo_tot=saldo_efe+saldo_tarj+saldo_aho;
     cout<<"el saldo total que tiene en su cuenta es de : "<<saldo_tot<<endl;
+    cout<<"******************************************* "<<endl;
+    cout<<"***** pagos efectuados del usuario ******** "<<endl;
+    cout<<"total gastado del usuario en comida "<<tot_comida<<endl;
+    cout<<"total gastado en agua o luz es de: "<<tot_agulu<<endl;
+    cout<<"total gastado en otros recurso es de: "<<tot_otros<<endl;
+    
+    cout<<"******************************************* "<<endl;
+    
     
     cout<<"______________________________"<<endl;
   	cout<<"digite 1: ingresos:"<<endl;
-  	cout<<"digite 2: retiro: "<<endl;
+  	cout<<"digite 2: pagos: "<<endl;
   	cout<<"digite 3: transferencia: "<<endl;
   	cout<<"digite 4: salir: "<<endl;
   	cout<<"digite su opcion: ";cin>>opc;
